d9e4b57a6d4033d6534a855613419afbe7d9e87d
张洒 <zhangsa@3tlife.cn>
2020-12-03 14:52:09 +0800

    面包屑

--- src/pages/info/baseInfo.vue
+++ src/pages/info/baseInfo.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        基本信息
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+        基本信息a
     </div>
 </template>
